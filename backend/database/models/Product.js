1034296a9c433cde7de1f2688aa0f6774d49a3b0
S2reds <110210946+S2reds@users.noreply.github.com>
2023-03-01 23:56:24 -0800

    feature: add product and review migrations

---
+++ backend/database/models/Product.js
@@ -0,0 +1,32 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Product extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+      Product.hasMany(models.Review, {foreignKey: "productId"})
+    }
+  }
+  Product.init({
+    id: DataTypes.INTEGER,
+    price: DataTypes.FLOAT,
+    name: DataTypes.STRING,
+    description: DataTypes.STRING,
+    countInStock: DataTypes.INTEGER,
+    numReviews: DataTypes.INTEGER,
+    picture: DataTypes.STRING,
+    category: DataTypes.STRING,
+    rating: DataTypes.FLOAT
+  }, {
+    sequelize,
+    modelName: 'Product',
+  });
+  return Product;
+};
